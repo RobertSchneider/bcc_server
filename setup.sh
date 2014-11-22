48eac374d1008046474c1c6cba9f46e50abe7c84
RobertSchneider <robert@schneider-apps.com>
2014-11-22 11:36:11 +0100

    Fixed bug

--- setup.sh
+++ setup.sh
@@ -14,4 +14,4 @@ mv /usr/local/include/json-c /usr/local/include/json
 make
 sudo cp libbcc.so /usr/local/lib/
 mkdir /usr/local/include/bcc
-sudo cp include/bcc.h /usr/local/lib/
+sudo cp include/bcc.h /usr/local/include/bcc/
